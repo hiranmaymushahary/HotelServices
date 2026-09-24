0e1330438ec2e1fd9b7adecf5df62e7b035764ba
Hiranmay Mushahary <hiranmaymushahary127@gmail.com>
2026-09-24 13:10:18 +0530

    Added roomGeneration service

--- src/repositories/roomCategory.repository.ts
+++ src/repositories/roomCategory.repository.ts
@@ -1,28 +1,8 @@
 import RoomCategory from "../db/models/roomCategory";
-import { NotFoundError } from "../utils/errors/app.error";
 import BaseRepository from "./base.repository";
 
-
-class RoomCategoryRepository extends BaseRepository<RoomCategory> {
-    constructor(){
+export class RoomCategoryRepository extends BaseRepository<RoomCategory> {
+    constructor() {
         super(RoomCategory);
     }
-
-
-    async findAllByHotelId(hotelId : number){
-        const roomcategories = await this.model.findAll({
-            where : {
-                hotelId : hotelId,
-                deletedAt : null
-
-            }
-        });
-
-        if(!roomcategories || roomcategories.length === 0){
-            throw new NotFoundError(`No categories for room were found with this id ${hotelId}`);
-        }
-
-        return roomcategories;
-    }
 }
-export default RoomCategoryRepository;
